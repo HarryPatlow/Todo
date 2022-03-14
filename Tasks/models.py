3cfddcdcbfddbd7c1b93d36fb1110ade4f470a07
Harry Patlow <ghp.coding@gmail.com>
2022-03-14 14:22:18 +0530

    Add Models

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=100)
+    priority = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    due_date = models.DateField()
+    due_time = models.TimeField()
+    notes = models.TextField()
+    done = models.BooleanField(default=False)
+
+
